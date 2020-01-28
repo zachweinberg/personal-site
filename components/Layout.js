31daa5769e0b5dd1e19193b47ab082e9c25640ce
Zach Weinberg <zjwgibsg@gmail.com>
2020-01-28 18:38:07 -0500

    view code link

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 
+import BracketsIcon from '../components/icons/Brackets'
 const Layout = ({ children }) => {
   return (
     <>
@@ -9,7 +10,14 @@ const Layout = ({ children }) => {
       <main className="antialiased flex flex-col min-h-screen max-w-2xl mx-auto p-8 text-white">
         {children}
         <footer className="text-center text-gray-700 mt-5">
-          Zach Weinberg &copy; {new Date().getFullYear()}
+          <p className="mb-3">Zach Weinberg &copy; {new Date().getFullYear()}</p>
+          <a
+            className="hover:text-gray-300 fade-color"
+            href="https://github.com/zachweinberg/zachweinberg.com"
+            target="_blank"
+          >
+            View Code <BracketsIcon className="inline fill-current w-4 h-4" />
+          </a>
         </footer>
       </main>
     </>
