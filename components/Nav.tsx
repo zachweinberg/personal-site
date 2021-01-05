e25c4117194269bedc27298e87b76c3abf8f3b21
Zach Weinberg <zweinberg3@gmail.com>
2021-01-05 18:26:07 -0500

    Margin

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const Nav: React.FunctionComponent = () => {
   return (
-    <nav className="flex items-center justify-end text-sm py-4">
+    <nav className="flex items-center justify-end text-sm py-4 mb-6 md:mb-0">
       <Link href="/">
         <a className="pr-2 md:pr-6 md:text-lg hover:text-gray-300 hover:text-gray-300">Home</a>
       </Link>
