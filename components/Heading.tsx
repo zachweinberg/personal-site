8163ce3b973591aeccec7d3ae03b7a9d6494e4e8
Zach Weinberg <zweinberg3@gmail.com>
2021-01-05 16:37:02 -0500

    Fix responsive text

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -4,7 +4,7 @@ interface Props {
 }
 
 const Heading: React.FunctionComponent<Props> = ({ title, subheading }: Props) => {
-  const size = subheading ? 'text-xl md:text-3xl' : 'text-3xl md:text-5xl'
+  const size = subheading ? 'text-xl md:text-3xl' : 'text-2xl sm:text-5xl'
   const Tag = subheading ? 'h2' : 'h1'
   return <Tag className={`font-bold ${size} tracking-tight mb-4`}>{title}</Tag>
 }
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,24 +2,28 @@ import Link from 'next/link'
 
 const Nav: React.FunctionComponent = () => {
   return (
-    <nav className="flex items-center justify-end py-2">
+    <nav className="flex items-center justify-end text-sm py-4">
       <Link href="/">
-        <a className="p-4 text-md hover:text-gray-300 hover:text-gray-300">Home</a>
+        <a className="pr-2 md:pr-6 md:text-lg hover:text-gray-300 hover:text-gray-300">Home</a>
       </Link>
       <Link href="/about">
-        <a className="p-4 text-md hover:text-gray-300">About Me</a>
+        <a className="pr-2 md:pr-6 md:text-lg hover:text-gray-300">About Me</a>
       </Link>
-      <a className="p-4 text-md hover:text-gray-300" href="https://github.com/zachweinberg" target="_blank">
+      <a
+        className="pr-2 md:pr-6 md:text-lg hover:text-gray-300"
+        href="https://github.com/zachweinberg"
+        target="_blank"
+      >
         GitHub
       </a>
       <a
-        className="p-4 text-md hover:text-gray-300"
+        className="pr-2 md:pr-6 md:text-lg hover:text-gray-300"
         href="https://www.linkedin.com/in/zachjamesweinberg"
         target="_blank"
       >
         LinkedIn
       </a>
-      <a className="p-4 text-md hover:text-gray-300" href="/resume.pdf" target="_blank">
+      <a className="pr-2 md:pr-6 md:text-lg hover:text-gray-300" href="/resume.pdf" target="_blank">
         Resume
       </a>
     </nav>
