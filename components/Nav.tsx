bf4d3d145294622ee196be8858ff418169e57670
Zach Weinberg <zweinberg3@gmail.com>
2021-01-17 01:12:24 -0500

    Fix mobile text

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -7,7 +7,7 @@ const Nav: React.FunctionComponent = () => {
         <a className="pr-3 md:pr-6 md:text-lg hover:text-gray-300">Home</a>
       </Link>
       <Link href="/about">
-        <a className="pr-3 md:pr-6 md:text-lg hover:text-gray-300">About Me</a>
+        <a className="pr-3 md:pr-6 md:text-lg hover:text-gray-300">About</a>
       </Link>
       <a
         className="pr-3 md:pr-6 md:text-lg hover:text-gray-300"
