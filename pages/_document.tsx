587c8dceeca5a2fc83d92ee4feb9460b64880f87
Zach Weinberg <zweinberg3@gmail.com>
2021-01-17 01:07:43 -0500

    Preload picture

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,7 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="preload" href="/fonts/karla.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
+          <link rel="preload" as="image" href="/img/zach.png" />
           <meta name="theme-color" content="#000" />
           <meta name="description" content="Zach Weinberg | Full Stack Web Engineer"></meta>
         </Head>
